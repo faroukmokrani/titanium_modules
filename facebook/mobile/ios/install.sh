d2b6bbcf89fc788563ab445a1ed8d3583a6f7699
xinhuixu <xinhui@MacBook-Pro-de-Xinhui.local>
2013-10-01 17:14:24 +0200

    update Facebook iOS sdk 3.8.0

---
+++ facebook/mobile/ios/install.sh
@@ -0,0 +1,4 @@
+unzip facebook-iphone-3.8.0.zip
+rm -rf ~/Dev/shopmium-mobile/Shopmium/modules/iphone/facebook/3.8.0
+cp -r  modules/iphone/facebook/3.8.0 ~/Dev/shopmium-mobile/Shopmium/modules/iphone/facebook
+
